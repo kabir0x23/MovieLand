d16594883ab7c5fdd910f73b8394dbe15c656858
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-14 19:26:37 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 
-const MovieCard = ({ movie, darkMode }) => {
+const MovieCard = ({movie, darkMode}) => {
 
-  return (
-    <div className={darkMode ? "movie-dark" : "movie-light"}>
-      <div>
-        <p>{movie.Year}</p>
-      </div>
-      <div>
-        <img
-          src={
-            movie.Poster !== "N/A"
-              ? movie.Poster
-              : "https://via.placeholder.com/400"
-          }
-          alt={"movie.Title"}
-        />
-      </div>
-      <div>
-        <span>{movie.Type}</span>
-        <h3>{movie.Title}</h3>
-        <h3 className={darkMode ? "year-dark" : "year-light"}>{movie.Year}</h3>
-      </div>
-    </div>
-  );
+    return (
+        <div className={darkMode ? "movie-dark" : "movie-light"}>
+            <a href={"https://www.imdb.com/title/" + movie.imdbID} target={"_blank"}>
+                <div>
+                    <h1>{movie.Year}</h1>
+                </div>
+                <div>
+                    <img src={movie.Poster !== "N/A" ? movie.Poster : "https://via.placeholder.com/400"}
+                         alt={"movie.Title"}/>
+                </div>
+                <div>
+                    <span>{movie.Type}</span>
+                    <h3>{movie.Title}</h3>
+                    {/*<h3 className={darkMode ? "year-dark" : "year-light"}>{movie.Year}</h3>*/}
+                </div>
+            </a>
+        </div>
+    )
+        ;
 };
 export default MovieCard;
